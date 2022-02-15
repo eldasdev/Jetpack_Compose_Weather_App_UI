6b48e7873f5343fb1d440caac3e51a6c57c85b30
eldasdev <tshavkat21@gmail.com>
2022-02-15 13:48:10 +0500

    Application First Version Has benn uploaded

---
+++ app/src/main/java/uz/eldasdev/weather_app_forgithub/ui/theme/Type.kt
@@ -0,0 +1,20 @@
+package uz.eldasdev.weather_app_forgithub.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import uz.eldasdev.weather_app_forgithub.R
+
+val CustomFont = FontFamily(
+    Font(R.font.futura_normal),
+    Font(R.font.futura_bold, FontWeight.Bold),
+    Font(R.font.futura_medium, FontWeight.Medium)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    defaultFontFamily = CustomFont
+)
